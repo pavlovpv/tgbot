cd5f3c04d639e01d3da6256383cba8692d67f6e4
Pavel Pavlov <pavlovpv5434@gmail.com>
2022-08-22 11:51:17 +0300

    add

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const cors = require("cors");
 
 let hello = require("./routes/hello");
+let main = require("./routes/main");
 
 const apiServer = express();
 
 apiServer.use(cors());
 
-apiServer.use(hello)
+apiServer.use(hello);
+apiServer.use(main);
 
 module.exports = apiServer;
